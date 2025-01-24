eccfce4b11a3194bd25c4728d08c84831b84f4c1
ElMariones <mariolan@ucm.es>
2025-01-24 03:46:46 +0100

    ruta imagenes test

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,39 +1,37 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
+import cards from '../utils/cards.js';
+import playButton from '../../assets/images/play.png';
+import submitBtn from '../../assets/images/submit.png';
+import shuffleBtn from '../../assets/images/shuffle.png';
+import rug from '../../assets/images/rug.png';
+
+// 3) Import audio
+import selectSound from '../../assets/audio/select.wav';
+import deselectSound from '../../assets/audio/deselect.wav';
+import shuffleSound from '../../assets/audio/shuffle.wav';
+import submitSound from '../../assets/audio/submit.wav';
 
 export default class BootScene extends Phaser.Scene {
-    constructor() {
-      super('BootScene');
-    }
-  
-    preload() {
-      const baseURL = './PokerInfinito/assets/'; // Adjust the base URL for GitHub Pages
-    
-      // 1) Card images
-      const suits = ['clubs','diamonds','hearts','spades'];
-      const ranks = ['2','3','4','5','6','7','8','9','10','jack','queen','king','ace'];
-      for (const suit of suits) {
-        for (const rank of ranks) {
-          this.load.image(`${rank}_of_${suit}`, `${baseURL}cards/${rank}_of_${suit}.png`);
-        }
-      }
-    
-      // 2) Images
-      this.load.image('playButton', `${baseURL}images/play.png`);
-      this.load.image('submitBtn', `${baseURL}images/submit.png`);
-      this.load.image('shuffleBtn', `${baseURL}images/shuffle.png`);
-      this.load.image('rug', `${baseURL}images/rug.png`);
-    
-      // 3) Audio files
-      this.load.audio('select', `${baseURL}audio/select.wav`);
-      this.load.audio('deselect', `${baseURL}audio/deselect.wav`);
-      this.load.audio('shuffle', `${baseURL}audio/shuffle.wav`);
-      this.load.audio('submit', `${baseURL}audio/submit.wav`);
-    }
-    
-  
-    create() {
-      // Start intro
-      this.scene.start('IntroScene');
-    }
+  preload() {
+    // Load all card images in a loop
+    Object.entries(cards).forEach(([key, path]) => {
+      this.load.image(key, path);
+    });
+
+    // 2) Other images
+    this.load.image('playButton', playButton);
+    this.load.image('submitBtn', submitBtn);
+    this.load.image('shuffleBtn', shuffleBtn);
+    this.load.image('rug', rug);
+
+    // 3) Audio files
+    this.load.audio('select', selectSound);
+    this.load.audio('deselect', deselectSound);
+    this.load.audio('shuffle', shuffleSound);
+    this.load.audio('submit', submitSound);
   }
-  
+
+  create() {
+    this.scene.start('IntroScene');
+  }
+}
