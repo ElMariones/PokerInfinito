221fff72cfd59947b9d12cb4cf8e1220e5590cb2
ManuUCM <manuvent@ucm.es>
2025-02-19 20:15:12 +0100

    Update DialogText.js

--- src/DialogText.js
+++ src/DialogText.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 
-export default class DialogText {
+export default class DialogText extends Phaser.Scene {
     constructor(scene, opts) {
         this.scene = scene;
         this.container = null;
@@ -8,8 +8,12 @@ export default class DialogText {
         this.currentIndex = -1;
         this.dialogLines = [];
         this.callback = null;
+        this.npc = null;
+        this.npc = data.npc;
+        this.currentStep = 0; 
+        this.transitionData = {};
 
-        this.init(opts);
+        this.create();
     }
 
     init(opts) {
@@ -23,6 +27,12 @@ export default class DialogText {
         this.fontFamily = opts.fontFamily || 'serif';
 
         this.createWindow();
+    }
+
+    create() {
+        this.scene.bringToTop();
+        this.gameScene = this.scene.get('MapScene');
+        this.init(opts);
     }
 
     createWindow() {
@@ -46,7 +56,56 @@ export default class DialogText {
         this.container.on('pointerdown', () => this.nextDialogLine());
     }
 
-    startDialog(dialogLines, background, onComplete) {
+    startDialog(npc, background, onComplete) {
+        this.npc = npc;
+        switch(npc) {
+            case 'samuel':
+                this.dialogLines = [
+                    { character: "Samuel", text: "Dante… no sabía si alguna vez vendrías. Apuesto a que tienes una carta dorada escondida en el bolsillo, ¿no es así?" },
+                    { character: "Dante", text: "¿Cómo sabes mi nombre?" },
+                    { character: "Samuel", text: "Eso no es lo importante. Si de verdad quieres saber lo que te come por dentro, tendrás que ganarme a mí y al resto. La paciencia es buena, pero si esperas demasiado, te quedas con las brasas. La vida es un juego, Dante, y el fuego siempre exige su turno." },
+                    { character: "Dante", text: "Espero no quemarme entonces." },
+                    { character: "Samuel", text: "Las cartas no mienten, Dante. Y esta mesa es mi trono. Si quieres un lugar en la historia de este pueblo, tendrás que arrebatármelo." }
+                ];
+                break;
+            case 'bruja':
+                this.dialogLines = [
+                    { character: "Helena", text: "Mira nada más… Dante Holloway, caminando entre las sombras. ¿Ya sabes lo que buscas o solo sigues las huellas de tu padre?" },
+                    { character: "Dante", text: "¿Tú también? ¿Cómo es que todos saben quién soy?" },
+                    { character: "Helena", text: "Dante, dulce Dante… si tienes esa carta dorada, entonces ya eres parte del juego. Ya debes saber por Samuel que lo único importante es ganar, avanzar y solo avanzar de local a local. Pero primero, tendrás que cruzar el Olvido." },
+                    { character: "Helena", text: "Las reglas son como el humo, querido… difíciles de atrapar. Lo mismo que mi juego." },
+                    { character: "Dante", text: "Entonces tendré que ver a través de la niebla." },
+                    { character: "Helena", text: "Mmm... eso me gusta. Juguemos, antes de que olvides por qué viniste." }
+                ];
+                break;
+            case 'gemelos':
+                this.dialogLines = [
+                    { character: "Hermano 1", text: "Míralo, hermano. Ahí está el chico con la carta dorada." },
+                    { character: "Hermano 2", text: "Oh sí… creía que tardaría más en aparecer." },
+                    { character: "Dante", text: "Son todos igual de insoportables." },
+                    { character: "Hermano 1", text: "Este ha salido peor que su padre. De igual manera querido Dante, si realmente quieres entrar al Casino, tendrás que demostrar que no solo eres un jugador… sino un estratega." },
+                    { character: "Hermano 2", text: "Aquí no basta con jugar bien, Dante." },
+                    { character: "Hermano 1", text: "Aquí necesitas pensar dos veces antes de cada jugada." },
+                    { character: "Hermano 2", text: "O mejor dicho… pensar como dos." }
+                ];
+                break;
+            case 'padre':
+                this.dialogLines = [
+                   //Falta poner
+                ];
+                break;
+            case 'pescador':
+                this.dialogLines = [
+                    { character: "Marco", text: "¡Ja! ¡Sabía que vendrías! Todos lo sabíamos. Ningún Holloway puede resistirse al brillo del Casino Ébano." },
+                    { character: "Dante", text: "Parece que mi reputación me precede." },
+                    { character: "Marco", text: "No es la reputación, muchacho. Es la carta dorada que arde en tu bolsillo. Pero antes de alzar las velas, tendrás que demostrar que sabes navegar estas aguas." },
+                    { character: "Dante", text: "Si si, estoy un poco harto de hablar con cada uno, empieza a barajar las cartas." },
+                    { character: "Marco", text: "La suerte es como el mar: caprichosa, despiadada... pero si sabes leer las corrientes, te lleva a la victoria" }
+                ];
+                break;
+            default:
+                this.dialogLines = [];
+        }
         this.dialogLines = dialogLines;
         this.currentIndex = 0;
         this.callback = onComplete;
@@ -58,6 +117,17 @@ export default class DialogText {
         this.container.setVisible(true);
         this.showText();
     }
+
+     // Nueva función para continuar el diálogo después de la batalla --Hay que reajustar
+    afterBattle(playerWon) {
+        const postBattleDialog = playerWon
+            ? [{ character: "Samuel", text: "Bien jugado… La paciencia cocina la mejor jugada, y tú la serviste en su punto. Ahora, ve a la Taberna del Olvido. Allí te espera Helena." }]
+            : [{ character: "Samuel", text: "Tienes la paciencia… pero no la precisión. Aquí, solo los reyes se sientan en la mesa final. Y hoy, no fue tu día." }];
+
+        this.dialogBox.startDialog(postBattleDialog, "asador_fondo", () => {
+            this.dialogBox.container.setVisible(false);
+        });
+}
     
     nextDialogLine() {
         this.currentIndex++;
