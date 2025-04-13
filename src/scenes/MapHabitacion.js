f5003e6dbbdaaeede0220dc2e7c5aae70a1210d5
pabcal <pabcal03@ucm.es>
2025-04-13 20:30:59 +0200

    Update MapHabitacion.js

--- src/scenes/MapHabitacion.js
+++ src/scenes/MapHabitacion.js
@@ -17,3 +17,35 @@ export default class MapHabitacion extends Phaser.Scene {
 
     // 2) Load your Tilemap and Tilesets
     const map = this.make.tilemap({ key: 'habitacionMap' });
+                                    //              tiledset    boot
+    const texturasBookShelf = map.addTilesetImage('bookShelf', 'bookShelf');
+    const texturasDrawerShelf = map.addTilesetImage('drawerShelf', 'drawerShelf');
+    const texturasHabitacionInside1 = map.addTilesetImage('habitacionInside1', 'habitacionInside1');
+    const texturasSuelos = map.addTilesetImage('suelos', 'suelos');
+    const texturasParedes = map.addTilesetImage('paredes', 'paredes');
+    const texturasHabitacionInside2 = map.addTilesetImage('habitacionInside2', 'habitacionInside2');
+    const texturasTV = map.addTilesetImage('TV', 'TV');
+    const texturasWindows = map.addTilesetImage('windows', 'windows');
+    const texturasTVBig = map.addTilesetImage('TVBig', 'TVBig');
+
+    // 3) Create layers
+// 3) Crear capas
+
+const todosLosTilesets = [
+  texturasBookShelf,
+  texturasDrawerShelf,
+  texturasHabitacionInside1,
+  texturasSuelos,
+  texturasParedes,
+  texturasHabitacionInside2,
+  texturasTV,
+  texturasWindows,
+  texturasTVBig
+];
+
+const layerSuelo = map.createLayer('suelo', todosLosTilesets, 0, 0);
+const layerPared = map.createLayer('wall', todosLosTilesets, 0, 0);
+const layerDecoracionPared = map.createLayer('walldecoration', todosLosTilesets, 0, 0);
+const layerVentanas = map.createLayer('windows', todosLosTilesets, 0, 0);
+const layerMobiliario = map.createLayer('furniture', todosLosTilesets, 0, 0);
+const layerEncimaMobiliario = map.createLayer('onTopOfFurniture', todosLosTilesets, 0, 0);
